39cdcd41e7e8dc93ca8f61b311647e0afd0cc349
marissavaneck <33225493+marissavaneck@users.noreply.github.com>
2022-05-30 10:28:59 +0200

    Add files via upload

---
+++ database/seeds/Bewoner.java
@@ -0,0 +1,12 @@
+public class Bewoner {
+    private String naam, leeftijd, huisnummer;
+
+    public String getBewoner(){
+        return bewoner1, bewoner2;
+    }
+    public String setBewoner(String nbewoner1, String nbewoner2){
+        bewoner1 = nbewoner1;
+        bewoner2 = nbewoner2
+    }
+
+}
